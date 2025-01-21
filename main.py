ec0e47e8a5806913dc99c3f872b2c38b18871d7b
Mohit Sharma <mohit1d.lp@gmail.com>
2025-01-21 21:17:20 +0530

    Added Model evaluation files and FASTAPI for app.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionPipeline
 from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationPipeline
 from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerPipeline
+from src.textSummarizer.pipeline.stage_4_model_evaluation_pipeline import ModelEvaluationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -33,6 +34,18 @@ try:
     logger.info(f"Stage: {STAGE_NAME} initiated")
     model_trainer_pipeline = ModelTrainerPipeline()
     model_trainer_pipeline.inititate_model_trainer()
+    logger.info(f"Stage: {STAGE_NAME} completed")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+
+try:
+    logger.info(f"Stage: {STAGE_NAME} initiated")
+    model_evaluation_pipeline = ModelEvaluationPipeline()
+    model_evaluation_pipeline.inititate_model_evaluation()
     logger.info(f"Stage: {STAGE_NAME} completed")
 
 except Exception as e:
